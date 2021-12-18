f50a593544b2eaf4858f38d6949534740810b8e8
M Athallariq Wiratama <61014055+yamz-11@users.noreply.github.com>
2021-12-18 22:15:25 +0700

    Add files via upload

---
+++ src/main/java/tugas/java/c/tugas/java/c/data/ktp/Application.java
@@ -0,0 +1,13 @@
+package tugas.java.c.tugas.java.c.data.ktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/tugas/java/c/tugas/java/c/data/ktp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas.java.c.tugas.java.c.data.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
